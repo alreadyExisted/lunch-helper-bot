dd711ba9541762ac2e98eb7f6450c315c057876b
alreadyExisted <vit.polyanskiy@gmail.com>
2020-01-25 22:07:32 +0200

    Add some events

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -18,17 +18,26 @@ export class BotService {
   }
 
   private registerClient() {
-    this.bot = new TelegramBot(this.configService.get('TELEGRAM_TOKEN'), { polling: true })
+    this.bot = new TelegramBot(this.configService.get('TELEGRAM_TOKEN'), {
+      polling: {
+        interval: 300,
+        autoStart: true,
+        params: {
+          timeout: 10
+        }
+      }
+    })
   }
 
   private registerEvents() {
     this.registerGroupChatCreated()
     this.registerMessage()
+    this.registerChatMembersChanges()
   }
 
   private registerMessage() {
     this.bot.on('message', msg => {
-      this.bot.sendMessage(msg.chat.id, `Привет ${msg.from.first_name}!`)
+      this.bot.sendMessage(msg.chat.id, `Спасибо ${msg.from.first_name} что написал ${msg.text}!`)
     })
   }
 
@@ -37,4 +46,27 @@ export class BotService {
       this.bot.sendMessage(msg.chat.id, 'Приветствую господа! Буду мониторить как вы ходите жрац!')
     })
   }
+
+  private registerChatMembersChanges() {
+    this.bot.on('new_chat_members', msg => {
+      const { new_chat_member: { first_name } } = (msg as unknown as Message)
+      this.bot.sendMessage(msg.chat.id, `Привет ${first_name}!`)
+    })
+    this.bot.on('left_chat_member', msg => {
+      const { left_chat_member: { first_name } } = (msg as unknown as Message)
+      this.bot.sendMessage(msg.chat.id, `Пока ${first_name}!`)
+    })
+  }
+}
+
+interface Message {
+  new_chat_member: Member
+  left_chat_member: Member
+}
+
+interface Member {
+  id: number
+  is_bot: boolean
+  first_name: string
+  last_name: string
 }
